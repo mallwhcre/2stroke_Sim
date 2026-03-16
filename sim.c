eae7e9cb5d7a8a125c3205c430fd98310fe09201
Mallwhcre <marykark.mk@gmail.com>
2026-03-16 22:05:58 +0200

    air pressure relative to piston position can be calculated

--- sim.c
+++ sim.c
@@ -1,44 +1,108 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-//yz125 2005
-const double bore = 54.0;
-const double stroke = 54.5;
-const double conRod = 102.0;// connecting rod length from center to center
-const double throw = stroke / 2.0; //connecting rod radius
-const double compRatio = 10.7;
+#define R_air 287.05   // mm^3
+#define T_atm 293.15   // 20 deg c in kelvin
+#define P_atm 101325.0 // atmospheric pressure in pascals
 
-double get_piston_pos(double crankAngle);
+#define BDC 180.0
+#define TDC 0.0
+// yz125 2005
 
-int main(){
+typedef struct {
 
-    //printf("Piston Position at TDC: %f\n", get_piston_pos(0.0));
-    //printf("Piston Position at BDC: %f\n", get_piston_pos(180.0));
+  double bore;
+  double stroke;
+  double conRod;
+  double throw;
+  double compRatio;
 
-    int revs=5;
+} EngineSpecs;
 
-    double totalAngle=0.0;
-    double angleStep=10.0;
-    double maxAngle=360.0*100;
+const EngineSpecs engine = {.bore = 54.0,
+                            .stroke = 54.5,
+                            .conRod = 102.0,
+                            .throw = 54.5 / 2.0,
+                            .compRatio = 10.7};
 
-    while(totalAngle< maxAngle){
-        double curAngle= fmod(totalAngle,360.0);
-        double pistonPos=get_piston_pos(curAngle);
-        printf("%.2f %.4f\n", curAngle, pistonPos);
-        totalAngle+=angleStep;
-    }
+double get_piston_pos(double crankAngle, EngineSpecs engine);
+double get_swept(EngineSpecs engine);
+double get_clearance(EngineSpecs engine);
+double get_volume(EngineSpecs engine, double crankAngle);
+double get_pressure(double crankAngle, double volume, double temperature,
+                    double mass);
+
+int main() {
+  double mass = (P_atm * (get_volume(engine, BDC) / 1e9)) / (R_air * T_atm);
+
+  int revs = 1;
+
+  double totalAngle = 0.0;
+  double angleStep = 10.0;
+  double maxAngle = 360.0 * revs;
+
+  while (totalAngle < maxAngle) {
+    double curAngle = fmod(totalAngle, 360.0);
+    double pistonPos = get_piston_pos(curAngle, engine);
+    printf("%.2f %.4f\n", curAngle, pistonPos);
+    printf("air pressure at %f is %f\n", curAngle,
+           get_pressure(curAngle, get_volume(engine, curAngle), T_atm, mass));
+    totalAngle += angleStep;
+  }
+
+  printf("the mass of the air is %f\n", mass);
+  // printf("the swept volume is %f\n", get_swept(engine));
+  // printf("the clearence volume is %f\n", get_clearance(engine));
+  // printf("the volume at 180 degrees is %f\n", get_volume(engine, 180));
 }
 
-double get_piston_pos(double crankAngle){
-    double h;
+double get_piston_pos(double crankAngle, EngineSpecs engine) {
+  double h;
 
-    double rad = crankAngle * M_PI / 180.0; // convert to radians
+  double rad = crankAngle * M_PI / 180.0; // convert to radians
 
-    h=conRod+throw*(1-cos(rad)) - sqrt( pow(conRod,2)-pow(throw*sin(rad),2) ); 
-    
-    return h;
+  h = engine.conRod + engine.throw *(1 - cos(rad)) -
+      sqrt(pow(engine.conRod, 2) - pow(engine.throw *sin(rad), 2));
+
+  return h;
+}
+
+double get_swept(EngineSpecs engine) {
+  double swept;
+
+  swept = 1 * (M_PI / 4) * pow(engine.bore, 2) * engine.stroke;
+
+  return swept;
+}
+
+double get_clearance(EngineSpecs engine) {
+  double clearance;
+
+  clearance = get_swept(engine) /
+              (engine.compRatio - 1); // clearance = swept / (CR − 1)
+
+  return clearance;
+}
+
+double get_volume(EngineSpecs engine, double crankAngle) {
+  double volume;
+  double pistonArea = (M_PI / 4) * pow(engine.bore, 2);
+  double pistonPos = get_piston_pos(crankAngle, engine);
+
+  volume = get_clearance(engine) + pistonArea * (pistonPos);
+
+  return volume;
+}
+
+double get_pressure(double crankAngle, double volume, double temperature,
+                    double mass) {
+  double pressure;
+
+  pressure = (mass * R_air * T_atm) / (volume / 1e9);
+
+  return pressure;
 }
