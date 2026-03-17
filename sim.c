615609b947b014904e518b20c6bd921b13822db6
Mallwhcre <marykark.mk@gmail.com>
2026-03-18 01:04:30 +0200

    Porting implemented, air pressure calculated

--- sim.c
+++ sim.c
@@ -11,6 +11,9 @@
 
 #define BDC 180.0
 #define TDC 0.0
+
+#define ePort 28.0 // mm from top
+#define tPort 41.0 // mm from top
 // yz125 2005
 
 typedef struct {
@@ -35,26 +38,31 @@ double get_clearance(EngineSpecs engine);
 double get_volume(EngineSpecs engine, double crankAngle);
 double get_pressure(double crankAngle, double volume, double temperature,
                     double mass);
+double get_port_angle(double portHeight, EngineSpecs engine);
 
 int main() {
   double mass = (P_atm * (get_volume(engine, BDC) / 1e9)) / (R_air * T_atm);
-
   int revs = 1;
 
   double totalAngle = 0.0;
-  double angleStep = 10.0;
+  double angleStep = 1.0;
   double maxAngle = 360.0 * revs;
 
   while (totalAngle < maxAngle) {
     double curAngle = fmod(totalAngle, 360.0);
     double pistonPos = get_piston_pos(curAngle, engine);
-    printf("%.2f %.4f\n", curAngle, pistonPos);
-    printf("air pressure at %f is %f\n", curAngle,
+
+    printf("crank angle is %10.4f  piston position is %10.6f  air pressure is "
+           "%14.2f\n",
+           curAngle, pistonPos,
            get_pressure(curAngle, get_volume(engine, curAngle), T_atm, mass));
+
     totalAngle += angleStep;
   }
 
-  printf("the mass of the air is %f\n", mass);
+  // printf("ePort angle is %f\n", ePort_angle);
+  // printf("tPort angle is %f\n", tPort_angle);
+
   // printf("the swept volume is %f\n", get_swept(engine));
   // printf("the clearence volume is %f\n", get_clearance(engine));
   // printf("the volume at 180 degrees is %f\n", get_volume(engine, 180));
@@ -102,7 +110,34 @@ double get_pressure(double crankAngle, double volume, double temperature,
                     double mass) {
   double pressure;
 
-  pressure = (mass * R_air * T_atm) / (volume / 1e9);
+  double ePort_angle = get_port_angle(ePort, engine);
+  double tPort_angle = get_port_angle(tPort, engine);
+
+  int ePort_open = 0;
+  int tPort_open = 0;
+
+  volume = get_volume(engine, crankAngle);
+
+  if (crankAngle > ePort_angle)
+    ePort_open = 1;
+  if (crankAngle > tPort_angle)
+    tPort_open = 1;
+
+  if (!ePort_open && !tPort_open)
+    pressure = (mass * R_air * T_atm) / (volume / 1e9);
+
+  else
+    pressure = P_atm;
 
   return pressure;
+}
+
+double get_port_angle(double portHeight, EngineSpecs engine) {
+  double angle;
+
+  angle = acos((pow((engine.conRod + engine.throw -portHeight), 2) -
+                pow(engine.conRod, 2) + pow(engine.throw, 2)) /
+               (2 * engine.throw *(engine.conRod + engine.throw -portHeight)));
+
+  return angle * 180 / M_PI;
 }
